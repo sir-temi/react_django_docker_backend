4969f879fd5933bb008a382fcd31c53b1e554fb6
TemiTope Kayode <engryankey@TemiTopes-MacBook-Pro.local>
2021-12-19 13:29:49 +0000

    Updated yml file

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ app_name = 'account'
 urlpatterns = [
     # path('', home, name='home'),
     # path('dashboard/', dashboard, name='dashboard'),
-    path('add_todo/', views.api_viewadd_todo, name='add_todo'),
+    path('add_todo/', views.add_todo, name='add_todo'),
     path('gettodos/', views.get_todos, name='get_todos'),
     path('deletetodos/<int:pk>/', views.delete_todo, name='delete_todo')
 ]
