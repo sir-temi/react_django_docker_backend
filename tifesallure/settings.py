ffe25545ad803b7dca7e3a70af70e9f38eb850ca
TemiTope Kayode <engryankey@TemiTopes-MacBook-Pro.local>
2021-12-19 13:07:04 +0000

    Updated yml file

--- tifesallure/settings.py
+++ tifesallure/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'tifesallure.wsgi.application'
 if os.getenv('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'github-actions',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
